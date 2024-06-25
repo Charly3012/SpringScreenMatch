29ba8b3779c764879fa773efed9749882b149a47
Carlos Coronado <carlocoronadosilva975@gmail.com>
2024-06-24 21:53:34 -0600

    Avanzando con el proyecto

--- src/main/java/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("sk-proj-3TVQhY7xXNDmg3mO0OzzT3BlbkFJzL57Oom0WddNSWmqWfZJ");
+        OpenAiService service = new OpenAiService("APIKEY");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
